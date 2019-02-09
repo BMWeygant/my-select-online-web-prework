7451e2bdba917cce601c815fb7e06b393f51cede
BRANDON M WEYGANT <bobbydigital108@gmail.com>
2019-02-09 04:31:38 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,11 +1,11 @@
 def my_select(array)
 i = 0
-my_selection = []
-while  i > array.length 
+select = []
+while  i > array.length
   yield(array[i])
-  my_selection << array[i]
+  select << array[i]
 end
   i = i + 1
 end
-my_selection
+select
 end
